149d476291e2c52175657ca7717653d72a910c4e
idapoppmo <206879@isd622.org>
2026-02-12 17:43:59 -0600

    added a comment

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -1,6 +1,6 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
+// the WPILib BSD license file in the root directory of this projec
 
 package frc.robot;
 
@@ -8,8 +8,9 @@ import edu.wpi.first.wpilibj.RobotBase;
 
 public final class Main {
   private Main() {}
-
+  //random comment
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
   }
 }
+//yufgfjyhf
